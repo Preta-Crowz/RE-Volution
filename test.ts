da8998bae0298c1a5139b356c6f2fc9583d5c7a5
Preta-Crowz <preta@siro.dev>
2020-12-04 13:44:06 +0900

    Delete test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { getFiles } from "https://deno.land/x/getfiles@v1.0.0/mod.ts"
-
-const files = [...getFiles("./Commands")]
-    .map((f) => f.name).sort();
-let content = "";
-for (const f of files) {
-  content += `export * from "./Commands${f}";\n`;
-}
-Deno.writeFileSync("../Data/Commands.ts", new TextEncoder().encode(content));
